0aa764bad36899ab32ca60af634fb1ca5dc1fd56
gerardoaranciaga <gerardoaranciaga60@gmail.com>
2020-09-10 18:07:07 -0300

    Listo

--- Tp1/js/tp1.js
+++ Tp1/js/tp1.js
@@ -2,44 +2,34 @@ let canvas = document.querySelector("#canvas");
 let ctx = canvas.getContext("2d");
 let rect = canvas.getBoundingClientRect();
 let x = 0; y = 0; dibujando = false; color = "black"; grosor = 1;
+ctx.lineWidth = 1;
+
+function dibujar(e){
+    x = e.clientX - rect.left;
+    y = e.clientY - rect.top;
+    if(dibujando == true){
+        ctx.lineTo(x,y);
+        ctx.stroke();
+    }
+}
+
+canvas.addEventListener("mousemove",dibujar);
+
+canvas.addEventListener("mousedown",function(){
+    dibujando = true;
+    ctx.beginPath();
+    ctx.moveTo(x,y);
+    canvas.addEventListener("mousemove",dibujar);
+});
+
+canvas.addEventListener("mouseup",function(){
+    dibujando = false;
+});
 
 function canvascolor(e){
-    color = e;
+    ctx.strokeStyle = e;
 }
 
 function canvasgrosor(e){
-    grosor = e;
-}
-
-canvas.addEventListener("mousedown",function(e){
-    x = e.clientX - rect.left;
-    y = e.clientY - rect.top;
-    dibujando = true;
-})
-
-canvas.addEventListener("mousemove",function(e){
-    if(dibujando === true){
-        dibujar(x,y,e.clientX - rect.left,e.clientY - rect.top);
-    }
-    x = e.clientX - rect.left;
-    y = e.clientY - rect.top;
-})
-
-canvas.addEventListener("mouseup",function(e){
-    if(dibujando === true){
-        dibujar(x,y,e.clientX - rect.left,e.clientY - rect.top);
-    }
-    x = 0;
-    y = 0;
-    dibujando = false;
-})
-
-function dibujar(x1,y1,x2,y2){
-    ctx.beginPath();
-    ctx.strokeStyle = color;
-    ctx.lineWidth = grosor;
-    ctx.moveTo(x1,y1);
-    ctx.lineTo(x2,y2);
-    ctx.stroke();
-    ctx.closePath();
+    ctx.lineWidth = e;
 }
