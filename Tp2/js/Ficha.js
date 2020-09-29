fbc101b4241eb973ffd4bde75a64d2e912931072
gerardoaranciaga <gerardoaranciaga60@gmail.com>
2020-09-29 09:07:55 -0300

    Actualizado

---
+++ Tp2/js/Ficha.js
@@ -0,0 +1,97 @@
+class Ficha{
+    constructor(posX,posY,radio,fill,context){
+        this.radio = radio;
+        this.posX = posX;
+        this.posY = posY;
+        this.fill = fill;
+        this.context = context;
+        this.seleccionada = false;
+        this.colorSeleccion = "red";
+        this.offsetX = 0;
+        this.offsetY = 0;
+    }
+
+    draw(){
+        this.context.fillStyle = this.fill;
+        this.context.beginPath();
+        this.context.arc(this.posX-this.offsetX,this.posY-this.offsetY,this.radio,0,2*Math.PI);
+        this.context.fill();
+        if(this.seleccionada === true){
+            this.context.strokeStyle = this.colorSeleccion;
+            this.context.lineWidth = 5;
+            this.context.stroke();
+        }
+        this.context.closePath();
+    }
+
+    estaAdentro(x,y){
+        let _x = this.posX -x;
+        let _y = this.posY -y;
+        if(Math.sqrt(_x * _x + _y * _y) < this.radio){
+            this.offsetX = x - this.posX;
+            this.offsetY = y - this.posY;
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+    getRadio(){
+        return this.radio;
+    }
+
+    offsetOff(){
+        this.posX = this.posX - this.offsetX;
+        this.posY = this.posY - this.offsetY;
+        this.offsetX = 0;
+        this.offsetY = 0;
+    }
+
+    setFill(fill){
+        this.fill = fill;
+    }
+
+    setSeleccionadaOn(){
+        this.seleccionada = true;
+    }
+    
+    setSeleccionadaOff(){
+        this.seleccionada = false;
+    }
+
+    getSeleccionada(){
+        return this.seleccionada;
+    }
+
+    getPosicion(){
+        return{
+            x: this.getPosX(),
+            y: this.getPosY()
+        };
+    }
+
+    getPosX(){
+        return this.posX;
+    }
+
+    getPosY(){
+        return this.posY;
+    }
+
+    setPosX(x){
+        this.posX = x;
+    }
+
+    setPosY(y){
+        this.posY = y;
+    }
+
+    setPosXY(x,y){
+        this.posX = x;
+        this.posY = y;
+    }
+
+    getFill(){
+        return this.fill;
+    }
+}
--- Tp2/js/main.js
+++ Tp2/js/main.js
@@ -2,43 +2,85 @@ let canvas = document.querySelector("#canvas");
 let ctx = canvas.getContext("2d");
 
 let moviendo = false;
-let figuraClickeada = null;
+let fichaClickeada = null;
 
-const maxFiguras = 50;
-const tamaño = 40;
-let figuras = [];
+const maxFichas = 50;
+const sectorFichas1 = 250;
+const sectorFichas2 = 1050;
+const tamañoFicha = 35;
+let fichas = [];
 
-addFiguras();
+inciarTablero();
 
-function addRectangulo(){
-    let posX = Math.round(Math.random() * canvas.width);
-    let posY = Math.round(Math.random() * canvas.height);
-    let color = randomRGBA();
-    let rectangulo = new Rectangulo(posX,posY,tamaño,tamaño,color,ctx);
-    figuras.push(rectangulo);
-}
-
-function addCirculo(){
-    let posX = Math.round(Math.random()* canvas.width);
-    let posY = Math.round(Math.random()* canvas.height);
-    let color = randomRGBA();
-    let circulo = new Circulo(posX,posY,25,color,ctx);
-    figuras.push(circulo);
-}
-
-function addFigura(){
-    if(Math.random() * 2 > 1){
-        addRectangulo();
+function addFicha(){
+    let posX = 0;
+    let posY = Math.round(Math.random() * (canvas.height));
+    if(posY < tamañoFicha){
+        posY += tamañoFicha;
+    }if(posY > canvas.height - tamañoFicha){
+        posY -= tamañoFicha;
+    }
+    if(fichas.length < (maxFichas/2)){
+        posX = Math.round(Math.random() * (sectorFichas1));
+        if(posX < tamañoFicha){
+            posX += tamañoFicha;
+        }if(posX > sectorFichas1 - tamañoFicha){
+            posX -= tamañoFicha;
+        }
     }else{
-        addCirculo();
+        posX = Math.round(Math.random() * (canvas.width - sectorFichas2) + sectorFichas2);
+        if(posX < sectorFichas2 + tamañoFicha){
+            posX += tamañoFicha;
+        }if(posX > canvas.width - tamañoFicha){
+            posX -= tamañoFicha;
+        }
+    }
+    let color = randomRGBA();
+    let ficha = new Ficha(posX,posY,tamañoFicha,color,ctx);
+    fichas.push(ficha);
+}
+
+
+function colorFicha(){
+    ctx.beginPath();
+    let img = new Image();
+    img.src = "../img/aguila.png";
+    //let imagen = ctx.createPattern(img,"repeat");
+    //ctx.arc(700,300,35,0,2*Math.PI);
+    //ctx.rect(0, 0, 150, 100);
+    //ctx.fillStyle = imagen;
+    //ctx.fill();
+    img.onload = ctx.drawImage(img,700,300,900,400);
+    ctx.closePath();
+}
+
+function drawFichas(){
+    clearCanvas();
+    for(let i = 0; i < fichas.length; i++){
+        fichas[i].draw();
     }
 }
 
-function drawFiguras(){
-    clearCanvas();
-    for(let i = 0; i < figuras.length; i++){
-        figuras[i].draw();
-    }
+function inciarTablero(){
+    addFichas();
+    drawTablero();
+}
+
+function drawTablero(){
+    drawFichas();
+    drawSectorFichas();
+    colorFicha();
+}
+
+function drawSectorFichas(){
+    ctx.beginPath();
+    ctx.lineWidth = 2;
+    ctx.moveTo(sectorFichas1,0);
+    ctx.lineTo(sectorFichas1,canvas.height);
+    ctx.moveTo(sectorFichas2,0);
+    ctx.lineTo(sectorFichas2,canvas.height);
+    ctx.stroke();
+    ctx.closePath();
 }
 
 function clearCanvas(){
@@ -46,23 +88,11 @@ function clearCanvas(){
     ctx.fillRect(0,0,canvas.width,canvas.height);
 }
 
-function addFiguras(){
-    for(i = 0; i < maxFiguras; i++){
-        addFigura();
-    }
-    drawFiguras();
-}
-
-/*function addFiguras(){
-    addFigura();
-    if(figuras.length < maxFiguras){
-        setTimeout(addFiguras,10);
+function addFichas(){
+    for(i = 0; i < maxFichas; i++){
+        addFicha();
     }
 }
-
-setTimeout(() => {
-    addFiguras();
-},100)*/
 
 function randomRGBA(){
     let r = Math.round(Math.random() * 255);
@@ -72,28 +102,18 @@ function randomRGBA(){
     return `rgb(${r},${g},${b},${a})`;
 }
 
-function buscarFiguraClickeada(x,y){
-    for(i = 0; i < figuras.length; i++){
-        let figura = figuras[i];
-        if(figura.estaAdentro(x,y)){
-            return figura;
+function buscarFichaClickeada(x,y){
+    for(i = 0; i < fichas.length; i++){
+        let ficha = fichas[i];
+        if(ficha.estaAdentro(x,y)){
+            return ficha;
         }
     }
 }
 
-/*canvas.addEventListener("click",function(e){
-    drawFiguras();
-    figuraClickeada = buscarFiguraClickeada(e.layerX,e.layerY);
-    if(figuraClickeada != null){
-        figuraClickeada.setSeleccionadaOn();
-        figuraClickeada.draw();
-        figuraClickeada.setSeleccionadaOff();
-    }
-})*/
-
 canvas.addEventListener("mousedown",function(e){
-    figuraClickeada = buscarFiguraClickeada(e.layerX,e.layerY);
-    if(figuraClickeada != null){
+    fichaClickeada = buscarFichaClickeada(e.layerX,e.layerY);
+    if(fichaClickeada != null){
         moviendo = true;
     }
 });
@@ -102,17 +122,17 @@ canvas.addEventListener("mousemove",function(e){
     let x = e.layerX;
     let y = e.layerY;
     if(moviendo == true){
-        figuraClickeada.setPosXY(x,y);
-        drawFiguras();  
+        fichaClickeada.setPosXY(x,y);
+        drawTablero();  
     }
 });
 
 canvas.addEventListener("mouseup",function(){
     moviendo = false;
-    if(figuraClickeada != null){
-        figuraClickeada.offsetOff();
+    if(fichaClickeada != null){
+        fichaClickeada.offsetOff();
     }
-    figuraClickeada = null;
+    fichaClickeada = null;
 });
 
 
